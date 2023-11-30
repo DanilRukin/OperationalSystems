10a248cd93a61718404f0af8ddb88e5522f46ebc
Rukin Danil <rukin2018d@gmail.com>
2023-11-30 23:01:51 +0700

    login view

--- lr_3/src/Messanger.Client/Presentation/Views/LoginView.cs
+++ lr_3/src/Messanger.Client/Presentation/Views/LoginView.cs
@@ -1,17 +1,57 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Messanger.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Messanger.Client.Presentation.Views
 {
     public class LoginView : IView
     {
+        private IServiceProvider _services;
+        private Presenter _presenter;
+
+        public LoginView(IServiceProvider services, Presenter presenter)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _presenter = presenter;
+        }
 
         public bool Show()
         {
-            throw new NotImplementedException();
+            string password;
+            string login;
+            IConfiguration config = _services.GetRequiredService<IConfiguration>();
+            int numberOfAttempts = Convert.ToInt32(config["NumberOfAttempts"]);
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($"Попытка залогиниться #{numberOfAttempts}");
+                Console.Write("\tИмя пользователя: ");
+                login = Console.ReadLine() ?? string.Empty;
+                Console.Write("\tПароль: ");
+                password = Console.ReadLine() ?? string.Empty;
+                using (var channel = GrpcChannel.ForAddress(config["ServerAddress"]))
+                {
+                    var client = new Messanger.MessangerClient(channel);
+                    var loginRequest = new LoginMessage() { Login = login, Password = password };
+                    var token = client.Login(loginRequest);
+                    if (Guid.Empty == Guid.Parse(token.Token))
+                    {
+                        _presenter.SetView(_services.GetRequiredService<MainView>());
+                        return true;
+                    }
+                }
+                numberOfAttempts--;
+            } while (numberOfAttempts > 0);
+            Console.WriteLine("Даун не смог залогиниться. Пошел нахер, черт..." +
+                "(нажми что-нибудь, скотина, чтобы я тебя больше не видел)");
+            Console.ReadKey(true);
+            return false;
         }
     }
 }
--- lr_3/src/Messanger.Client/Presentation/Views/SelectInterlocutorView.cs
+++ lr_3/src/Messanger.Client/Presentation/Views/SelectInterlocutorView.cs
@@ -11,9 +11,30 @@ namespace Messanger.Client.Presentation.Views
     /// </summary>
     public class SelectInterlocutorView : IView
     {
+        private IServiceProvider _services;
+        private Presenter _presenter;
+
+        public SelectInterlocutorView(IServiceProvider services, Presenter presenter)
+        {
+            _services = services ?? throw new ArgumentNullException(nameof(services));
+            _presenter = presenter ?? throw new ArgumentNullException(nameof(presenter));
+        }
+
         public bool Show()
         {
-            throw new NotImplementedException();
+            ConsoleKey key;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Введите номер друга, с которым хотите пообщаться," +
+                    "либо нажмите ESC для выхода");
+                key = Console.ReadKey(true).Key;
+                if (key != ConsoleKey.Escape)
+                {
+
+                }
+            } while (key != ConsoleKey.Escape);
+            return true;
         }
     }
 }
